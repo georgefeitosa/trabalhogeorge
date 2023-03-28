832ced75804d97e068e01d459ca3e4bf40705dbf
George Feitosa <128500872+georgefeitosa@users.noreply.github.com>
2023-03-28 15:56:43 -0300

    Update trabalho av1 ambientes .js

--- trabalho av1 ambientes .js
+++ trabalho av1 ambientes .js
@@ -1,66 +1,109 @@
-function isPrime(n) {
-  if (n < 2) {
-    return false;
-  }
-  for (let i = 2; i <= Math.sqrt(n); i++) {
-    if (n % i === 0) {
-      return false;
-    }
-  }
-  return true;
+
+function primeNumber(num) {
+    for (var divisor = 2; divisor < num; divisor++) 
+    if (num % divisor == 0) return false;
+    return true;
 }
 
-function sum(numbers) {
-  let total = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    total += numbers[i];
-  }
-  return total;
-}
-function fibonacci(n) {
-  if (n < 1) {
-    return [];
-  } else if (n === 1) {
-    return [0];
-  } else if (n === 2) {
-    return [0, 1];
-  } else {
-    let fib = [0, 1];
-    for (let i = 2; i < n; i++) {
-      fib.push(fib[i - 1] + fib[i - 2]);
+var determinadoNumero = 50;
+
+for (var i = 2; i < determinadoNumero; i++) if (primeNumber(i)) console.log(i);
+
+
+
+function sum(input){
+             
+    if (toString.call(input) !== "[object Array]")
+       return false;
+         
+               var total =  0;
+               for(var i=0;i<input.length;i++)
+                 {                  
+                   if(isNaN(input[i])){
+                   continue;
+                    }
+                     total += Number(input[i]);
+                  }
+             return total;
+               }
+   console.log(sum([8,9,10]));
+
+
+
+   let cache = {};
+
+   const fibonacci = n => {
+     if (n <= 1) {
+       return n;
+     }
+     
+     if(cache[n]) {
+       return cache[n];
+     }
+     
+     const result = fibonacci(n - 1) + fibonacci(n - 2);
+     
+     cache[n] = result;
+     
+     return result;
+   };
+   console.log(fibonacci(12))   
+
+
+
+
+   function calcularMDC(num1, num2) {
+    var resto, x, y;
+    x = num1;
+    y = num2;
+    while(resto!=0){
+      resto = x % y;
+      x = y;
+      y = resto;
     }
-    return fib;
-  }
+    return (num1*num2)/x;
+  };
+var resultado = calcularMDC(5, 10);
+console.log(calcularMDC(7,8));
+
+
+
+
+let numbers = [5, 7, 8, 8, 9, 22, 13];
+let count = [];
+let i, z = 0;
+let max = Math.max(...numbers);      
+for (i = 0; i <= max; i++) {
+    count[i] = 0;
 }
-function gcd(a, b) {
-  if (b === 0) {
-    return a;
-  } else {
-    return gcd(b, a % b);
-  }
+for (i=0; i < numbers.length; i++) {
+    count[numbers[i]]++;
 }
-function quickSort(array) {
-  if (array.length <= 1) {
-    return array;
-  }
-  const pivot = array[0];
-  const left = [];
-  const right = [];
-  for (let i = 1; i < array.length; i++) {
-    if (array[i] < pivot) {
-      left.push(array[i]);
-    } else {
-      right.push(array[i]);
+for (i = 0; i <= max; i++) {
+    while (count[i]-- > 0) {
+        numbers[z++] = i;
     }
-  }
-  return quickSort(left).concat(pivot, quickSort(right));
 }
-function countInRange(numbers, n) {
-  let count = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] >= numbers[0] && numbers[i] <= n) {
-      count++;
+for (i=0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
+
+
+function countIntegersBetweenFirstAndN(N, dataSet) {
+    let count = 0;
+    const firstValue = dataSet[0];
+  
+    for (let i = firstValue; i <= N; i++) {
+      if (Number.isInteger(i) && dataSet.includes(i)) {
+        count++;
+      }
     }
+  
+    return count;
   }
-  return count;
-}
+  
+  const dataSet = [0,2,6.4,7,8];
+  const N = 20;
+  const integerCount = countIntegersBetweenFirstAndN(N, dataSet);
+  
+  console.log(`Há ${integerCount} números inteiros entre ${dataSet[0]} e ${N}.`);
